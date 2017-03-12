e8cb279eed5f0b6cbc35a37ac2942524477e05c1
Marcin Gorecki <marccin95@gmail.com>
2017-03-12 08:07:13 +0100

    dzien 1

--- Hallo2/src/Hallo.java
+++ Hallo2/src/Hallo.java
@@ -1,9 +1,27 @@
 
 public class Hallo {
 
+	int aa;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello!!");
+		// syso ctr+spacja println
+		// nazwa zmiennej alt+shift+r zmienia we wszystkich miejscach
+		//ctrl+1 do podpowiedzi
+		//przy zaznaczonej linii alt+shift+m do stworzenia metody
+		//ctrl+o pokazuje budowê kodu
+		//ctrl+w zamyka otwarty plik
+		//ctrl+shift+p do otwierania klas
+		
+		double kokoko = 0.1;
+		System.out.println(kokoko);
+		String string = new String();
+		
+		String takie_tam = "blablabla";
+		wyswietl(args, takie_tam);
+		System.out.println("Hello!!" + args[1]);
+	}
+
+	private static void wyswietl(String[] args, String takie_tam) {
+		System.out.println(takie_tam+"Hello!!" + args[0]);
 	}
 
 }
